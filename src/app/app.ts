27d585e1d3d230790c92954bea95bdbc4131f792
Varsha Pandian <varsha.pandian@gmail.com>
2026-05-27 15:36:38 +0530

    about component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from "./components/home/home";
+import { AboutComponent } from "./components/about/about";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
+  imports: [RouterOutlet, Home, AboutComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/about/about.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-about',
+  imports: [CommonModule],
+  templateUrl: './about.html',
+  styleUrl: './about.css'
+})
+export class AboutComponent implements OnInit {
+
+  @ViewChild('aboutCard', { static: true }) aboutCard!: ElementRef;
+
+  skills = {
+  languages: [
+    'Java',
+    'SQL',
+    'Python',
+    'TypeScript',
+    'JavaScript'
+  ],
+
+  frameworks: [
+    'Spring Boot',
+    'Angular',
+    'REST APIs',
+  ],
+
+  tools: [
+    'Git',
+    'GitHub',
+    'Postman',
+    'JMeter',
+    'MySQL',
+  ],
+
+  interests: [
+    'Backend Engineering',
+    'Scalable System Design',
+    'Microservices Architecture',
+    'Distributed Systems',
+    'Full-Stack Development',
+    'Generative AI',
+    'Agentic AI'
+  ],
+
+  hobbies: [
+    'Building Side Projects',
+    'Graphic Designing',
+    'Public Speaking',
+    'Classical Dance'
+  ]
+};
+
+  ngOnInit() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        }
+      });
+    }, { threshold: 0.1 });
+
+    observer.observe(this.aboutCard.nativeElement);
+  }
+}
